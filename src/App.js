debc64ec958f86cdbd51c19228b4c2f6bd5e1f99
roninstack101 <yashurathore2001@gmail.com>
2025-06-26 17:35:17 +0530

    white page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Grid from './Component/Header/bentogrid';
+// import Grid from './Component/Header/bentogrid';
+import Sidebar from './Component/Header/whitesidebar';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+  
+      <Sidebar/>
+      <Grid/>
+   
     </div>
   );
 }
